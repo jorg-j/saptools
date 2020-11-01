e770588c1df610e3b501d12be2c5092ca19367a7
jorg-j <47870433+jorg-j@users.noreply.github.com>
2020-11-02 07:18:07 +1100

    add comments

--- Example.py
+++ Example.py
@@ -1,11 +1,16 @@
+'''
+Opens T-Code VA01
+'''
+
 import sys
 import saptools
 
+# Connection to SAP
 session = saptools.SAPConnect()
 
 try:
-    session.findById("wnd[0]").maximize()
-    session.findById("wnd[0]/tbar[0]/okcd").text = "/nVA01"
-    session.findById("wnd[0]").sendVKey (0)
+    session.findById("wnd[0]").maximize() # SAP Session is Maximised
+    session.findById("wnd[0]/tbar[0]/okcd").text = "/nVA01" # Key T-Code to toolbar
+    session.findById("wnd[0]").sendVKey (0) # Press Enter
 except:
     print(sys.exc_info()[0])
