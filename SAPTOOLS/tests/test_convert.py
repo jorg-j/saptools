0aa49a7f6f7faa98d9cec0393fd0c0acb7476ad9
Jack <jorgensen.jack@gmail.com>
2021-04-02 18:35:15 +1100

    added unit tests

---
+++ SAPTOOLS/tests/test_convert.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+
+import unittest
+import SAPConverter as SC
+
+
+class TestSum(unittest.TestCase):
+
+    def test_needsParenthesis(self):
+        needsParenthesis = [
+        '.press', '.maximize',
+        '.doubleClickCurrentCell',
+        '.select', '.setFocus'
+        ]
+        for item in needsParenthesis:
+            expect = f'check{item}()'
+            self.assertEqual(SC.vbsConverter(f'check{item}'), expect, f'Should be {expect}')
+        
+    def test_vbsConvert(self):
+        testString =  'session.findById("wnd[0]").sendVKey 0'
+        expect = 'session.findById("wnd[0]").sendVKey (0)'
+        self.assertEqual(SC.vbsConverter(testString), expect, f'Should be {expect}')
+
+
+if __name__ == '__main__':
+    unittest.main()
